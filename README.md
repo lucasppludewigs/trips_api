34b15e09b6e925a910de644a6949dd9745fb5779
Lucas Ludewigs <lucas.ludewigs@gmail.com>
2022-12-17 19:13:09 -0300

    added interaction with database

--- README.md
+++ README.md
@@ -1,6 +1,26 @@
 # Trips API
 
+The problem statement of this challenge can be found in the pdf file.
+
 ## Usage
+
+Initialize the database:  
+```
+docker pull postgres
+docker run -p 5432:5432 --name trip-postgres -e POSTGRES_PASSWORD=postgres -d postgres
+docker exec -it trip-postgres psql -U postgres
+create table trips (
+ region varchar(100),
+ origin_coord varchar(200),
+ destination_coord varchar(200),
+ datetime timestamp,
+ datasource varchar(100)
+);
+```
+
+To be able to install psycopg2, we need postgres: `brew install postgresql`
+
+Install the dependencies on requirements.txt: `pip install requirements.txt`
 
 Run the flask python API:
 
--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 from flask import Flask, request
 import pandas as pd
+import psycopg2
 
 app = Flask(__name__)
 
-# TODO: consider migrating to Fast API which is more robust
+conn = psycopg2.connect(user="postgres", password="postgres", host="localhost")
 
 @app.route('/')
 def hello_world():
-    print('Hello')
-    return 'Hello world!'
+    print('Hello!')
+    return "I'm working!"
 
 @app.route('/ingest', methods=['POST'])
 def ingest_trip():
@@ -16,19 +17,22 @@ def ingest_trip():
     trip_file = request.form['trip_file']
     # vaex + HDF5 might be better than pandas
     df = pd.read_csv(trip_file)
+    cursor = conn.cursor()
     for index, row in df.iterrows():
-        print(index,row)
+        print(f"index: {index}, row: {row}")
+        print(f"this is row datasource: {row['datasource']}")
+        cursor.execute(f"INSERT INTO TRIPS VALUES ('{row['region']}','{row['origin_coord']}','{row['destination_coord']}','{row['datetime']}','{row['datasource']}');")
+    conn.commit()
+    cursor.close()
     return f'index: {index}, row {row}',201
 
-print("I'm working")
-
-# # Data Model
-
-# region varchar(100)
-# origin_coord varchar(200)
-# destination_coord varchar(200)
-# datetime datetime
-# datasource varchar(100)
+# TODO: consider migrating to Fast API which is more robust
+# TODO: check database persistency
+# TODO: data model
+# TODO: use alembic for database version and schema evolution
+# TODO: encapsulate functions
+# TODO: test these functins
+# TODO: black formatter
 
 # partition keys: similar origin, destin, time of day
 # https://stackoverflow.com/questions/37689554/postgres-partition-by-character-prefix
